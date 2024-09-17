fb047c7006c6a7c6ea44cb7572d07ed2ad5f9eb3
matiasraimundez <163176870+matiasraimundez@users.noreply.github.com>
2024-09-17 10:29:04 -0300

    Update Eval3.hs

--- src/Eval3.hs
+++ src/Eval3.hs
@@ -13,17 +13,17 @@ type State = (M.Map Variable Int, String)
 
 -- Estado vacío
 initState :: State
-initState = (M.empty, [])
+initState = (M.empty, "")
 
 -- Busca el valor de una variable en un estado
 lookfor :: Variable -> State -> Either Error Int
 lookfor v (m, _) = case M.lookup v m of
-                        Nothing -> Left UndefVar
-                        Just n  -> Right n
+                      Nothing -> Left UndefVar
+                      Just n  -> Right n
 
 -- Construye una string para mostrar las expresiones Let
 showLet :: Variable -> Int -> String
-showLet v n = "Let " ++ v ++ " " ++ show n ++ "\n"
+showLet v n = " / Let " ++ v ++ " " ++ show n
 
 -- Cambia el valor de una variable en un estado
 update :: Variable -> Int -> State -> State
@@ -53,20 +53,20 @@ stepComm Skip s = Right (Skip :!: s)
 
 -- Evalúa comando de asignación
 stepComm (Let v e) s = case evalExp e s of
-                            Left err         -> Left err
-                            Right (n :!: s') -> Right (Skip :!: update v n s')
+                          Left err         -> Left err
+                          Right (n :!: s') -> Right (Skip :!: update v n s')
 
 -- Evalúa composición secuencial
 stepComm (Seq Skip c1) s = Right (c1 :!: s)
-stepComm (Seq c0   c1) s = case stepComm c0 s of
-                                    Left err           -> Left err
-                                    Right (c0' :!: s') -> Right (Seq c0' c1 :!: s')
+stepComm (Seq c0 c1)   s = case stepComm c0 s of
+                              Left err           -> Left err
+                              Right (c0' :!: s') -> Right (Seq c0' c1 :!: s')
 
 -- Evalúa ejecución condicional
 stepComm (IfThenElse b c0 c1) s = case evalExp b s of
-                                       Left err             -> Left err
-                                       Right (True  :!: s') -> Right (c0 :!: s')
-                                       Right (False :!: s') -> Right (c1 :!: s')
+                                      Left err             -> Left err
+                                      Right (True  :!: s') -> Right (c0 :!: s')
+                                      Right (False :!: s') -> Right (c1 :!: s')
 
 -- Evalúa ciclos
 stepComm r@(RepeatUntil c b) s = Right (Seq c (IfThenElse b Skip r) :!: s)
@@ -74,54 +74,52 @@ stepComm r@(RepeatUntil c b) s = Right (Seq c (IfThenElse b Skip r) :!: s)
 -- Evalúa operador unario
 evalUnOp :: (t -> a) -> Exp t -> State -> Either Error (Pair a State)
 evalUnOp op e s = case evalExp e s of
-                           Left err         -> Left err
-                           Right (n :!: s') -> Right (op n :!: s')
+                      Left err         -> Left err
+                      Right (n :!: s') -> Right (op n :!: s')
 
 -- Evalúa operador binario
 evalBinOp :: (t1 -> t2 -> a) -> Exp t1 -> Exp t2 -> State -> Either Error (Pair a State)
 evalBinOp op e0 e1 s = case evalExp e0 s of
-                            Left err          -> Left err
-                            Right (n0 :!: s') -> case evalExp e1 s' of
-                                                      Left err           -> Left err
-                                                      Right (n1 :!: s'') -> Right (op n0 n1 :!: s'')
+                          Left err          -> Left err
+                          Right (n0 :!: s') -> case evalExp e1 s' of
+                                                  Left err           -> Left err
+                                                  Right (n1 :!: s'') -> Right (op n0 n1 :!: s'')
 
 -- Evalúa división
 evalDiv :: Exp Int -> Exp Int -> State -> Either Error (Pair Int State)
 evalDiv e0 e1 s = case evalExp e0 s of
-                       Left err          -> Left err
-                       Right (n0 :!: s') -> case evalExp e1 s' of
-                                                 Left err           -> Left err
-                                                 Right (n1 :!: s'') -> if n1 == 0 
-                                                                         then Left DivByZero 
-                                                                         else Right (div n0 n1 :!: s'')
+                      Left err          -> Left err
+                      Right (n0 :!: s') -> case evalExp e1 s' of
+                                              Left err           -> Left err
+                                              Right (n1 :!: s'') -> if n1==0 then Left DivByZero else Right (div n0 n1 :!: s'')
 
 -- Evalúa una expresión
 evalExp :: Exp a -> State -> Either Error (Pair a State)
 
 -- Expresiones enteras
-evalExp (Const nv   ) s = Right (nv :!: s)
-evalExp (Var x      ) s = case lookfor x s of
-                               Left err -> Left err
-                               Right n  -> Right (n :!: s)
-evalExp (VarInc x   ) s = case lookfor x s of
-                               Left err -> Left err
-                               Right n  -> Right ((n+1) :!: update x (n+1) s)
-evalExp (VarDec x   ) s = case lookfor x s of
-                               Left err -> Left err
-                               Right n  -> Right ((n-1) :!: update x (n-1) s)
-evalExp (UMinus e0  ) s = evalUnOp  negate e0    s
-evalExp (Plus  e0 e1) s = evalBinOp (+)    e0 e1 s
+evalExp (Const nv)    s = Right (nv :!: s)
+evalExp (Var x)       s = case lookfor x s of
+                              Left err -> Left err
+                              Right n  -> Right (n :!: s)
+evalExp (VarInc x)    s = case lookfor x s of
+                              Left err -> Left err
+                              Right n  -> Right ((n+1) :!: update x (n+1) s)
+evalExp (VarDec x)    s = case lookfor x s of
+                              Left err -> Left err
+                              Right n  -> Right ((n-1) :!: update x (n-1) s)
+evalExp (UMinus e0)   s = evalUnOp  negate e0    s
+evalExp (Plus e0 e1)  s = evalBinOp (+)    e0 e1 s
 evalExp (Minus e0 e1) s = evalBinOp (-)    e0 e1 s
 evalExp (Times e0 e1) s = evalBinOp (*)    e0 e1 s
-evalExp (Div   e0 e1) s = evalDiv          e0 e1 s
+evalExp (Div e0 e1)   s = evalDiv          e0 e1 s
 
 -- Expresiones booleanas
 evalExp BTrue       s = Right (True  :!: s)
 evalExp BFalse      s = Right (False :!: s)
-evalExp (Lt  e0 e1) s = evalBinOp (<)  e0 e1 s
-evalExp (Gt  e0 e1) s = evalBinOp (>)  e0 e1 s
+evalExp (Lt e0 e1)  s = evalBinOp (<)  e0 e1 s
+evalExp (Gt e0 e1)  s = evalBinOp (>)  e0 e1 s
 evalExp (And e0 e1) s = evalBinOp (&&) e0 e1 s
-evalExp (Or  e0 e1) s = evalBinOp (||) e0 e1 s
-evalExp (Not e0   ) s = evalUnOp  not  e0    s
-evalExp (Eq  e0 e1) s = evalBinOp (==) e0 e1 s
+evalExp (Or e0 e1)  s = evalBinOp (||) e0 e1 s
+evalExp (Not e0)    s = evalUnOp  not  e0    s
+evalExp (Eq e0 e1)  s = evalBinOp (==) e0 e1 s
 evalExp (NEq e0 e1) s = evalBinOp (/=) e0 e1 s
